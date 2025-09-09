507544d2f74949299f3ae6189c2998ad12473b2d
chouzzy <mathfernando@hotmail.com>
2025-09-09 16:24:09 -0300

    stripe version

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 
 // Inicializa o cliente do Stripe com a sua chave secreta
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: '2025-07-30.basil',
+    apiVersion: '2025-08-27.basil',
 });
 
 /**
