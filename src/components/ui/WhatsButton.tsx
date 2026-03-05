2ba8bf438de1e53974b5eb0373754a81c5912891
chouzzy <mathfernando@hotmail.com>
2026-03-05 10:47:39 -0300

    remoção do link no zap button

--- src/components/ui/WhatsButton.tsx
+++ src/components/ui/WhatsButton.tsx
@@ -20,15 +20,14 @@ const handleWhatsAppClick = () => {
 export default function WhatsButton() {
     
     return (
-        <Link className="zap-tag" target={'_blank'} onClick={() => handleWhatsAppClick()}>
-        
-        <Flex zIndex={1} bg='#25D366' color="#fffafa"
+        <Flex 
+        onClick={() => handleWhatsAppClick()}
+        zIndex={1} bg='#25D366' color="#fffafa"
         fontSize='1.8rem' borderRadius={'full'} p={4} 
         position='fixed'bottom={8} right={8}
         _hover={{ transition:' 400ms', fontSize:'2.4rem'}}
         cursor='pointer'>
             <BsWhatsapp/>
         </Flex>
-        </Link>
     )
 }
